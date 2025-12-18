4c1a89ad81cc9c8063320243495848a26293d008
Sayuru Akash Amarasinghe <sayuru555@gmail.com>
2025-12-18 13:53:33 +0530

    chore: Downgrade Laravel framework from v11 to v10, updating core application files and dependencies.

--- routes/console.php
+++ routes/console.php
@@ -5,5 +5,5 @@ use Illuminate\Foundation\Console\ClosureCommand;
 
 // Run hourly to ensure messages are sent even if the server was down at midnight.
 // The command handles duplicate checking internally, so this is safe.
-Schedule::command('birthday:send')->hourly();
+// Schedule::command('birthday:send')->hourly();
 
--- routes/web.php
+++ routes/web.php
@@ -1,30 +1,32 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\LoginController;
 use App\Http\Controllers\ContactController;
+use App\Http\Controllers\BirthdayController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
 
 Route::get('/', function () {
-    return redirect()->route('login');
+    return view('welcome');
 });
 
-Route::get('/login', [LoginController::class, 'showLoginForm'])->name('login');
-Route::post('/login', [LoginController::class, 'login'])->name('login.post');
-Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
+Route::middleware(['auth'])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
 
-Route::middleware(['admin.auth'])->group(function () {
     Route::resource('contacts', ContactController::class);
-    Route::post('contacts/import', [ContactController::class, 'import'])->name('contacts.import');
-    Route::get('contacts/export/csv', [ContactController::class, 'export'])->name('contacts.export');
-    Route::post('contacts/send-today', [ContactController::class, 'sendToday'])->name('contacts.send_today');
-    Route::post('contacts/bulk-delete', [ContactController::class, 'bulkDestroy'])->name('contacts.bulk_delete');
-    Route::post('contacts/delete-all', [ContactController::class, 'deleteAll'])->name('contacts.delete_all');
-    Route::post('contacts/{contact}/send-manual', [ContactController::class, 'sendManual'])->name('contacts.send_manual');
-    Route::get('logs', [App\Http\Controllers\ActivityLogController::class, 'index'])->name('logs.index');
 });
 
-use App\Http\Controllers\BirthdayController;
 Route::get('/birthday/{token}', [BirthdayController::class, 'show'])->name('birthday.show');
-Route::get('/birthday/{token}/card', [BirthdayController::class, 'card'])->name('birthday.card');
-Route::get('/birthday/{token}/download', [BirthdayController::class, 'download'])->name('birthday.download');
 
+// require __DIR__.'/auth.php';
