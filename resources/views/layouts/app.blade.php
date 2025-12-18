4507c0b93e4294fcfd1d8da1389851e0504e5847
Sayuru Akash Amarasinghe <sayuru555@gmail.com>
2025-12-18 13:38:57 +0530

    feat: Add favicon.ico to public directory.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="robots" content="noindex, nofollow">
+    <link rel="icon" href="{{ asset('favicon.ico') }}">
     <title>Birthday Automator - SITC Campus</title>
     @vite(['resources/css/app.css', 'resources/js/app.js'])
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
